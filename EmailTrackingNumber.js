5f4c21b24dd0690564e65918f260e412f29b8fc6
Wassim Taher <wassim.taher@hotmail.com>
2015-10-05 13:20:59 +0300

    Update EmailTrackingNumber.js

--- EmailTrackingNumber.js
+++ EmailTrackingNumber.js
@@ -1,43 +1,87 @@
 (function() {
-    var board_id, css,
+    var board_id,
         _this = this;
 
     board_id = window.board.model.id;
 
-    css = " .contextMenu li.change_column >a { background-image: url('/images/icons/silk/note_go.png'); }        .change_column_opts li a { background-image: url('/images/icons/silk/note.png');  } ";
+    
 
     if (window.board.model.users.get(KanbanTool.Board.current_user_id).can('move_tasks', window.board.model)) {
-        $('<style>').html(css).appendTo('head');
+        
         $.ajax({
             type: 'GET',
             url: '/boards/' + parseInt(board_id + 20) + '/schema.json',
             async: false,
             dataType: 'json',
             success: function(schema) {
+                
+                var myShippedColumnID;
+                
                 var columns, max_width;
                 max_width = 120;
-                columns = '<ul class="level2 change_column_opts">';
+
                 $(schema.workflow_stages).each(function(i, e) {
                     var width;
-                    columns += '<li><a href="#extension:change_column:' + e.id + '">' + e.full_name + '</a></li>';
+
                     width = e.full_name.length * 6 + 30;
                     if (width > max_width) {
                         return max_width = width;
                     }
+                    
+                    if (e.full_name == "Shipped"){
+                        myShippedColumnID = e.id;
+                    }
+                    
                 });
-                columns += '</ul>';
-                $('#task_context_menu .change_board').after('<li class="change_column has_submenu"><a onclick="return false;" href="#">Change column Now</a>' + columns + '</li>');
-                return $('.change_column_opts').css({
-                    'width': max_width
-                });
+
+
+                $('#task_context_menu .change_board').after('<li><a href="#extension:email-tracking-number:' + myShippedColumnID + '">Ship & Email</a></li>');
+                
             }
         });
         $(window).bind('extensionContextMenuAction', function(event, action, el, pos) {
             var current_ws_id, task_id, ws_id;
-            if (/extension:change_column:([0-9])/.test(action)) {
-                ws_id = action.replace(/extension:change_column:/, '');
+            if (/extension:email-tracking-number:([0-9])/.test(action)) {
+                ws_id = action.replace(/extension:email-tracking-number:/, '');
                 current_ws_id = $(el).parent()[0].id.split('_')[1];
                 task_id = $(el)[0].id.split('_')[1];
+                
+                
+                
+                if (ws_id !== current_ws_id) {
+                    // Sending email - START
+                        
+                        
+                        var myTrackingNumber = "";
+                        var myEmailAddress = "";
+                        
+                        var myCardContent = $(el)[0].innerHTML;
+                        
+                        var a = myCardContent.split("<li>"), i;
+                        
+                        for (i = 1; i < a.length; i++) {
+                            
+                            var myInnerCardContent = a[i].split("</li>");
+                            
+                            if (i==a.length-2){
+                                myTrackingNumber = myInnerCardContent[0];
+                            }
+                            
+                            if (i== a.length-1){
+                                myEmailAddress = myInnerCardContent[0];
+                            }
+                            
+                            
+                        }
+                        
+                        var subject = "We sent your shipment";
+                        var body = "Your tracking code is: " + myTrackingNumber;
+                        window.location.href = "mailto:" + myEmailAddress + "?subject=" + subject + "&body=" + body;
+                        
+                    // Sending email - END
+                }
+                
+                
                 if (ws_id !== current_ws_id) {
                     return $.ajax({
                         type: 'POST',
@@ -52,6 +96,8 @@
                         }
                     });
                 }
+                
+                
             }
         });
     }
